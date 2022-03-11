03dba79e9edbc9c0d2c3c4f0fa5ec341f3cf9c77
krysor <krysor@a.fritz.box>
2022-03-11 11:34:48 +0100

    memmove good

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,33 +6,36 @@
 /*   By: kkaczoro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 10:42:33 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/03/10 18:36:54 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/03/11 11:33:40 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
+#include <stdio.h>
+
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	(void)dest;
-	(void)src;
-	(void)n;
-	/*
 	int		i;
 	char	*temp;
 
 	i = 0;
-	while (i < (int)n)
+	temp = (char *)src;
+	if (src > dest)
 	{
-		temp[i] = ((char *)src)[i];
-		i++;
+		while (i < (int)n)
+		{
+			((char *)dest)[i] = temp[i];
+			i++;
+		}	
 	}
-	i = 0;
-	while (i < (int)n)
+	else
 	{
-		((char *)dest)[i] = temp[i];
-		i++;
+		while (i < (int)n)
+		{
+			((char *)dest)[n - 1 - i] = temp[n - 1 - i];
+			i++;
+		}	
 	}
-	*/
 	return (dest);
 }
