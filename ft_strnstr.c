59ef84aad95badb956a30d3cbbf9735fb2f7f090
krysor <krysor@a.fritz.box>
2022-03-12 21:36:29 +0100

    strnstr finished

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: kkaczoro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 19:38:40 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/03/12 21:05:48 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/03/12 21:35:36 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	ind_lst;
 	char	*str;
 
+	str = (char *)big;
 	ind_lst = ft_strlen(little);
 	if (ind_lst == 0)
-		return (big);
+		return (str);
 	ind_lst -= 1;
 	i = 0;
-	str = (char *)big;
 	while (i < len && str[i])
 	{
 		j = 0;
-		while (str[i + j] == little[j])
+		while (i + j < len && str[i + j] == little[j])
 		{
 			if (j == ind_lst)
 				return (str + i);
