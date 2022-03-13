871325b65003fceb5380fa937b147c3bfec8b37d
krysor <krysor@a.fritz.box>
2022-03-13 17:58:39 +0100

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -1,31 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kkaczoro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/12 23:48:08 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/03/13 00:03:47 by kkaczoro         ###   ########.fr       */
+/*   Created: 2022/03/13 17:43:34 by kkaczoro          #+#    #+#             */
+/*   Updated: 2022/03/13 17:58:15 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
-	int		i;
+	int		ind;
 
-	str = malloc(sizeof(char) * ft_strlen(s) + 1);
+	str = malloc(sizeof(char) * len + 1);
 	if (str == NULL)
 		return (NULL);
-	i = 0;
-	while (s[i])
+	ind = 0;
+	while (len && s[start])
 	{
-		str[i] = s[i];
-		i++;
+		str[ind++] = s[start++];
+		len--;
 	}
-	str[i] = '\0';
+	str[ind] = '\0';
 	return (str);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kkaczoro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 15:14:24 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/03/12 23:49:58 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/03/13 17:44:49 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
