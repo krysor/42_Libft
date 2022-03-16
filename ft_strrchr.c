fc459c0011909c9adbfb0b5352534413d6b4c1e0
krysor <krysor901@gmail.com>
2022-03-16 14:56:53 +0100

    ft_strrchr.c corrected

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: kkaczoro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 16:57:39 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/03/12 19:36:25 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/03/16 14:56:24 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ char	*ft_strrchr(const char *s, int c)
 	int	i;
 
 	i = ft_strlen(s);
-	if (s[i] == c)
+	if (((unsigned char *)s)[i] == (unsigned char)c)
 		return ((char *)s + i);
 	i--;
 	while (i >= 0 && s[i])
 	{
-		if (s[i] == c)
+		if (((unsigned char *)s)[i] == (unsigned char)c)
 			return ((char *)s + i);
 		i--;
 	}
