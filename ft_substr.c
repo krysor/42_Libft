c8577263417c485ac2e4677d39bd412d8d63ac76
krysor <krysor901@gmail.com>
2022-03-16 16:06:21 +0100

    ft_substr.c corrected

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: kkaczoro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 17:43:34 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/03/15 15:49:08 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/03/16 16:06:05 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	int		ind;
+	size_t	len_s;
 
+	len_s = ft_strlen(s);
+	if (start > len_s)
+		return (ft_strdup(""));
+	len_s -= start;
+	if (len > len_s)
+		len = len_s;
 	str = malloc(sizeof(char) * (len + 1));
 	if (str == NULL)
 		return (NULL);
 	ind = 0;
-	while (len && s[start])
+	while (len)
 	{
 		str[ind++] = s[start++];
 		len--;
