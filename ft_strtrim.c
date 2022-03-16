d8ff9757b0bb0ea5cd42227aab705bac6294586d
krysor <krysor901@gmail.com>
2022-03-16 19:30:04 +0100

    mandatory all tests passed

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: kkaczoro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 21:06:17 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/03/16 18:57:49 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/03/16 19:21:39 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,8 @@ static int	ft_ind_end(char const *s1, char const *set)
 		else
 			j++;
 	}
+	if (i == -1)
+		return (0);
 	return (i);
 }
 
@@ -64,9 +66,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if ((s1 == NULL && set == NULL) || (s1 == NULL || set == NULL))
 		return (NULL);
 	i_beg = ft_ind_beg(s1, set);
-	i_end = ft_ind_end(s1, set);
-	if (i_beg > i_end)
+	if (i_beg == ft_strlen(s1))
 		return (ft_strdup(""));
+	i_end = ft_ind_end(s1, set);
 	str = malloc(sizeof(char) * (2 + i_end - i_beg));
 	if (str == NULL)
 		return (NULL);
