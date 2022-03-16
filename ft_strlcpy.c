6dbdcd585ee028a334389eb0d6bd197bcae88a93
krysor <krysor901@gmail.com>
2022-03-16 15:14:09 +0100

    ft_strlcpy.c corrected

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: kkaczoro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 11:38:56 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/03/12 18:45:31 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/03/16 15:13:55 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	if (size != 0)
+	if (size > 0)
 	{
-		while (i < ((int)size - 1) && src[i])
+		while (i < (size - 1) && src[i])
 		{
 			dst[i] = src[i];
 			i++;
